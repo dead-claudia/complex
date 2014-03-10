ec69c49dccd31e8c3b793732d00b0dc4afbf71b9
impinball <impinball@gmail.com>
2014-03-10 15:10:41 -0400

    add several methods, optimize some

--- complex.js
+++ complex.js
@@ -10,7 +10,7 @@ var MyTypeError = TypeError; // alias to help minify
 var type        = Object.prototype.toString.call;
 
 var isNaN = function (num) { // override global method for here
-  return (type(num) !== Number || !isFinite(num));
+  return !(type(num) === Number || !isFinite(num));
 };
 
 // Automatically return if not ES5 or later
@@ -76,6 +76,8 @@ if (Math['hypot']) {
   var hypot = Math['hypot'];
 } else {
   // This is a full implementation of Math.hypot, excluding the length property
+  // TODO: convert to gist
+  /*
   var hypot = function (x, y, z) {
     var args = arguments.toArray();
     var length = args.length;
@@ -120,56 +122,56 @@ if (Math['hypot']) {
       while (length--) {
         element   = array.pop();
         childComb = getCombs(array, n - 1);
-        for (j = childComb.length - 1; j--; ) {
-          ret.push(element[0].concat(childComb[j]));
+        j = childComb.length - 1;
+        while (j) {
+          ret.push(element.concat(childComb[j--]));
         }
       }
       
       return ret;
-    }
-    var lastTerm    = args.pop();
-    var encodedArgs = [];
-    for (var i in args) encodedArgs.push([i]);
-    var combinations = getCombs(encodedArgs, length - 1);
+    };
+    
+    var lastTerm     = args.pop();
+    var combinations = getCombs(args, length - 1);
     var combLength   = combinations.length - 1;
     var sum          = 0;
-    var product      = 1;
+    var prod         = 1;
+    var combProd     = 1;
     
     for (var i in args) prod *= i;
     
-    for (var i = combLength, combProd = 1; i--; ) {
-      for (var j in combinations[i]) combProd *= j;
+    while (i) {
+      for (var j in combinations[i--]) combProd *= j;
       
       sum += 1 / combProd / combProd;
     }
     return abs(prod) * sqrt(sum + x / prod);
   };
+  */
+  var hypot = function (x, y) {
+    return x * sqrt(1 + (y /= x) * y);
+  }
 }
 
 function exportFunctions() {
-  ComplexNumber.fn['toString'] = ComplexNumber.fn.toString;
-  ComplexNumber.fn['plus']     = ComplexNumber.fn.plus;
-  ComplexNumber.fn['minus']    = ComplexNumber.fn.minus;
-  ComplexNumber.fn['times']    = ComplexNumber.fn.times;
-  ComplexNumber.fn['divide']   = ComplexNumber.fn.divide;
-  ComplexNumber.fn['recip']    = ComplexNumber.fn.recip;
-  ComplexNumber.fn['arg']      = ComplexNumber.fn.arg;
-  ComplexNumber.fn['real']     = ComplexNumber.fn.real;
-  ComplexNumber.fn['imag']     = ComplexNumber.fn.imag;
-  ComplexNumber.fn['conj']     = ComplexNumber.fn.conj;
-  ComplexNumber.fn['negate']   = ComplexNumber.fn.negate;
-  ComplexNumber.fn['exp']      = ComplexNumber.fn.exp;
-  ComplexNumber.fn['sin']      = ComplexNumber.fn.sin;
-  ComplexNumber.fn['cos']      = ComplexNumber.fn.cos;
-  ComplexNumber.fn['tan']      = ComplexNumber.fn.tan;
-  ComplexNumber.fn['abs']      = ComplexNumber.fn.abs;
-  ComplexNumber.fn['length']   = ComplexNumebr.fn.length;
-  ComplexNumber.fn['sqrt']     = ComplexNumber.fn.sqrt;
-  
-  ComplexNumber.prototype = ComplexNumber.fn;
-  
-  // remove this alias to limit extra footprint
-  delete ComplexNumber.fn;
+  ComplexNumber.prototype['toString'] = ComplexNumber.prototype.toString;
+  ComplexNumber.prototype['plus']     = ComplexNumber.prototype.plus;
+  ComplexNumber.prototype['minus']    = ComplexNumber.prototype.minus;
+  ComplexNumber.prototype['times']    = ComplexNumber.prototype.times;
+  ComplexNumber.prototype['divide']   = ComplexNumber.prototype.divide;
+  ComplexNumber.prototype['recip']    = ComplexNumber.prototype.recip;
+  ComplexNumber.prototype['arg']      = ComplexNumber.prototype.arg;
+  ComplexNumber.prototype['real']     = ComplexNumber.prototype.real;
+  ComplexNumber.prototype['imag']     = ComplexNumber.prototype.imag;
+  ComplexNumber.prototype['conj']     = ComplexNumber.prototype.conj;
+  ComplexNumber.prototype['negate']   = ComplexNumber.prototype.negate;
+  ComplexNumber.prototype['exp']      = ComplexNumber.prototype.exp;
+  ComplexNumber.prototype['sin']      = ComplexNumber.prototype.sin;
+  ComplexNumber.prototype['cos']      = ComplexNumber.prototype.cos;
+  ComplexNumber.prototype['tan']      = ComplexNumber.prototype.tan;
+  ComplexNumber.prototype['abs']      = ComplexNumber.prototype.abs;
+  ComplexNumber.prototype['length']   = ComplexNumebr.prototype.length;
+  ComplexNumber.prototype['sqrt']     = ComplexNumber.prototype.sqrt;
   
   global['ComplexNumber'] = ComplexNumber;
 }
@@ -180,7 +182,7 @@ var ComplexNumber = function (real, imaginary) {
   this.im = imaginary;
 };
 
-ComplexNumber.fn = {
+ComplexNumber.prototype = {
   constructor: ComplexNumber,
   
   // the length property of this object is two
@@ -217,6 +219,16 @@ ComplexNumber.fn = {
   
   imag: function (num) {
     return (def(num)) ? num.im : this.im;
+  },
+  
+  realToComplex: function (num) {
+    if (def(this)) num = this;
+    return realToComplex(num);
+  },
+  
+  imagToComplex: function (num) {
+    if (def(this)) num = this;
+    return imagToComplex(num);
   },
   
   plus: function (numOne, numTwo) {
@@ -346,17 +358,53 @@ ComplexNumber.fn = {
     var real = num.re;
     if (!imag) return realToComplex(tan(real));
     if (!real) return imagToComplex(tanh(imag));
-    return sin(num).divide(cos(num));
+    return num.sin().divide(num.cos());
   },
   
   log: function (num) {
     if (def(this)) num = this;
     var real = num.re;
     if (!imag) return realToComplex(log(real));
-    return newComplexNumber(Math.log(abs(num)), arg(num));
+    return newComplexNumber(Math.log(num.abs()), num.arg());
   },
   
+  sqrt: function (num) {
+    if (def(this)) num = this;
+    var r = num.abs();
+    var t = num.angle() / 2;
+    return newComplexNumber(r * cos(t), r * sin(t));
+  },
   
+  sinh: function (num) {
+    if (def(this)) num = this;
+    var re   = num.re;
+    var im   = num.im;
+    var real = sinh(re);
+    var imag = sin(im);
+    if (!im) return realToComplex(real);
+    if (!re) return imagToComplex(imag);
+    return newComplexNumber(real * cos(im), imag * sinh(re));
+  },
+  
+  cosh: function (num) {
+    if (def(this)) num = this;
+    var re   = num.re;
+    var im   = num.im;
+    var real = cosh(re);
+    var imag = cos(im);
+    if (!im) return realToComplex(real);
+    if (!re) return realToComplex(imag);
+    return newComplexNumber(real * imag, sinh(re) * sin(im));
+  },
+  
+  tanh: function (num) {
+    if (def(this)) num = this;
+    var real = num.re;
+    var imag = num.im;
+    if (!im) return realToComplex(tanh(real));
+    if (!re) return imagToComplex(tan(imag));
+    return num.sin().divide(num.cos());
+  }
 };
 
 exportFunctions();
